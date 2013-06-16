46406e7f0b37084227ada43b7d832e4da6e51b5d
ribbon-xx <mr.ribbon@gmail.com>
2013-06-16 18:05:43 +0700

    GREEN shouldReturnNumberOnNumberInput

--- src/com/qsoft/longdt/StringCalculator.java
+++ src/com/qsoft/longdt/StringCalculator.java
@@ -2,14 +2,15 @@ package com.qsoft.longdt;
 
 public class StringCalculator {
 
-	public static int add(String input){
+	public static int add(String input) {
 		if (input.isEmpty()) {
 			return 0;
+		} else if (input.length() == 1) {
+			return Integer.parseInt(input);
 		}
 		return -1;
 	}
-	
-	
+
 	/**
 	 * @param args
 	 */
