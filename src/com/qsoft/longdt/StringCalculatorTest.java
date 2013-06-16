b0efe77631fd980ec65579c6814cd23054d07846
ribbon-xx <mr.ribbon@gmail.com>
2013-06-16 18:21:21 +0700

    RED shouldReturnWithDelimDefine

--- src/com/qsoft/longdt/StringCalculatorTest.java
+++ src/com/qsoft/longdt/StringCalculatorTest.java
@@ -37,5 +37,10 @@ public class StringCalculatorTest {
 		exception.expectMessage("negatives not allowed -3,-5");
 		StringCalculator.add("1,-3,-5");
 	}
+	
+	@Test
+	public void shouldReturnWithDelimDefine() {
+		assertEquals(3, StringCalculator.add("//;\n1;2"));
+	}
 
 }
