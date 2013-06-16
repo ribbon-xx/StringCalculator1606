aab1ca1ae208c1eb90fa50b76e0eda88c5408c2b
ribbon-xx <mr.ribbon@gmail.com>
2013-06-16 18:11:07 +0700

    GREEN shouldReturnWithCommaDelim

--- src/com/qsoft/longdt/StringCalculator.java
+++ src/com/qsoft/longdt/StringCalculator.java
@@ -1,5 +1,9 @@
 package com.qsoft.longdt;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
 	public static int add(String input) {
@@ -7,12 +11,30 @@ public class StringCalculator {
 			return 0;
 		} else if (input.length() == 1) {
 			return toInt(input);
+		} else if (input.contains(",")) {
+			return methodSum(input);
 		}
 		return -1;
 	}
 
 	private static int toInt(String input) {
 		return Integer.parseInt(input);
+	}
+
+	private static int methodSum(String input) {
+		ArrayList<Integer> ints = new ArrayList<Integer>();
+		Pattern p = Pattern.compile("-?\\d+");
+		Matcher m = p.matcher(input);
+		while (m.find()) {
+			int number = toInt(m.group());
+			ints.add(number);
+		}
+
+		int sum = 0;
+		for (Integer integer : ints) {
+			sum += integer;
+		}
+		return sum;
 	}
 
 	/**
